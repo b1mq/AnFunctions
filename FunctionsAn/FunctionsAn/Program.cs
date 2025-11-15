bc707cd8a11b186f1ca3989e1da8f1a64cf8f0f7
Egor <arnoldtagirov1@gmail.com>
2025-11-15 16:33:28 +0100

    -m

--- FunctionsAn/FunctionsAn/Program.cs
+++ FunctionsAn/FunctionsAn/Program.cs
@@ -9,3 +9,23 @@
 //var NumbersNegativeAndUnikum = Mylist.FindAll(x => x >= 0);
 //Func <string,string,bool> containsWord = (text,word)  => text.Contains(word);
 //var UniqueNums = (int[] a) => a.Where(x => x < 0).Distinct();
+
+
+int[] data = {1,2,3,4,5,6,7,8,9,10 };
+
+Func<int[], int[]> EvenNumbersInArray = array => array.Where(x => x % 2 == 0).ToArray();
+Func<int[], int[]> OddNumbersInArray = array => array.Where(x => x % 2 != 0).ToArray();
+Func<int[], int[]> PrimeNums = array => array.Where(n =>
+    n > 1 &&
+    Enumerable.Range(2, (int)Math.Sqrt(n)).All(i => n % i != 0)
+).ToArray();
+
+Action ShowTimeAction = () => Console.WriteLine($"Time: {DateTime.Now.ToString("HH:mm:ss")}");
+Action ShowDayOfTheWeekAction = () => Console.WriteLine($"Day: {DateTime.Now.DayOfWeek}");
+Action ShowDataAction = () => Console.WriteLine($"Data: {DateOnly.FromDateTime(DateTime.Now).ToString()}");
+
+Func<double,double,double> TriangleS = (a,b) => 0.5 * a * b ;
+Func<double,double,double> SquareS = (a,b) =>  a * b ;
+
+int[] Evenum = EvenNumbersInArray(data);
+int[] Oddnum = OddNumbersInArray(data);
